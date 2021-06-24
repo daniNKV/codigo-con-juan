da093f218b6d3aecb10c1490eaf765d23fa91079
Dansen <nikiforovdaniel@gmail.com>
2021-06-24 20:19:23 -0300

    create

---
+++ 05-IntroduccionJS/js/01.js
@@ -0,0 +1,9 @@
+// Variables //
+
+
+//Estilos de variable
+
+var nombre_producto = "mouse" ; //underscore
+var nombreProducto = "mouse" ; //CamelCase
+var NombreProducto = "mouse" ; //PascalCase
+var nombreproducto = ""
